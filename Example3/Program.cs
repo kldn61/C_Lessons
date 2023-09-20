136f3809e4104c0cb20a1850ace07f339d5fde7f
Vladislav Koldunov <kldn61@gmail.com>
2023-09-20 16:03:18 +0300

    Added Ex3

---
+++ Example3/Program.cs
@@ -0,0 +1,9 @@
+﻿//Показать частное двух чисел
+
+// int numberA = 27;
+// int numberB =12;
+// Console.WriteLine(numberA / numberB);
+
+double numberA = 27;
+double numberB =12;
+Console.WriteLine(numberA / numberB);
