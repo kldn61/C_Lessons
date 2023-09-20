e603f58cd8275da85588d2091e494a6c4d2b59b5
Vladislav Koldunov <kldn61@gmail.com>
2023-09-20 18:38:18 +0300

    Ex6

---
+++ Example6/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача с гирями
+
+int a = 3;
+int b = 1;
+int c = 8;
+int d = 5;
+int f = 6;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (f > max) max = f;
+
+Console.Write("MAX = ");
+Console.WriteLine(max);
