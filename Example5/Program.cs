9f92a073632e43afab631859658cbdf843486237
Vladislav Koldunov <kldn61@gmail.com>
2023-09-20 18:13:35 +0300

    Ex5 Hello User

--- Example5/Program.cs
+++ Example5/Program.cs
@@ -1,2 +1,13 @@
 ﻿// Создание программы по приветствию любимчика
 
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username == "Helen")
+{
+    Console.WriteLine("ПРИВЕТ КРАСОТКА!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
