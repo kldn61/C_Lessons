38a07bd732e84d2b27e07e16b2acb682a17238a8
Vladislav Koldunov <kldn61@gmail.com>
2023-09-20 18:21:00 +0300

    Ex5 CHANGE

--- Example5/Program.cs
+++ Example5/Program.cs
@@ -1,8 +1,20 @@
 ﻿// Создание программы по приветствию любимчика
 
+// Console.Write("Введите имя пользователя: ");
+// string username = Console.ReadLine();
+// if(username == "Helen")
+// {
+//     Console.WriteLine("ПРИВЕТ КРАСОТКА!");
+// }
+// else
+// {
+//     Console.Write("Hello, ");
+//     Console.WriteLine(username);
+// }
+
 Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
-if(username == "Helen")
+if(username.ToLower() == "helen")
 {
     Console.WriteLine("ПРИВЕТ КРАСОТКА!");
 }
